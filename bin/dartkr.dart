76d6cee2ce658c24e1dbf31a498ee3422b424928
pc <a@mail>
2023-09-30 10:36:44 +0500

    final commit

--- bin/dartkr.dart
+++ bin/dartkr.dart
@@ -1,9 +1,27 @@
-//задание: создать проект на дарте, инициализировать репозиторий, 
+//задание: создать проект на дарте, инициализировать репозиторий,
 //изначальное сразу комитим. когда заканчивает делаем снова комит 2 вариант получаем список продуктов
 //переменные назвать по смыслу, любую строчку кода объяснить
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) { 
+void main(List<String> arguments) async {
+  final url = Uri.http('dummyjson.com',
+      '/products'); //собираем из переданных строк uri(уникальный идентификатор ресурса)
 
-  
+  final response = await http.get(url); //делаем запрос на http
 
+  Map<String, dynamic> jsonData = convert.jsonDecode(response.body); //
+
+  List<dynamic> products = jsonData["products"]; //создаем список продуктов
+
+  double sumofallratings = 0.0;
+
+  products.forEach((p) {
+    //в forEach передаем функцию, которая считай рейтинг всех продуктов
+    var ratingone = p['rating']; //считываем рейтинг одного продукта
+    sumofallratings += ratingone; //суммируем
+  });
+
+  double average = sumofallratings / products.length; //вычисляем среднее
+  print("average rating: $average");
 }
